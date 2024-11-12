47beaf7d176ad0c3b6b6b5a1aa3e31fc8dbd2dab
Alexpbunea <apereb07@estudiantes.unileon.es>
2024-11-12 14:03:41 +0100

    Primera prueba del indexer

--- alexander/src/main/java/org/example/Indexer.java
+++ alexander/src/main/java/org/example/Indexer.java
@@ -1,24 +1,20 @@
 package org.example;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.codecs.Codec;
@@ -32,28 +28,40 @@ import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
 import org.apache.lucene.search.similarities.Similarity;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-
-
-
+import org.apache.lucene.store.*;
 
 
 public class Indexer {
-    private void indexDocs(Directory directory, Codec codec) throws IOException {
-        Analyzer defaultAnalyzer = new StandardAnalyzer();
-        CharArraySet stopWords = new CharArraySet(Arrays.asList("in", "dei", "di"), true);
-        Map<String, Analyzer> perFieldAnalyzers = new HashMap<>();
-        perFieldAnalyzers.put("contenuto", new StandardAnalyzer(stopWords));
-        perFieldAnalyzers.put("titolo", new WhitespaceAnalyzer());
 
-        Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        if (codec != null) {
-            config.setCodec(codec);
+    public void index(String filePath,List<String> nombres ,List<String> titulos, List<String> autores, List<String> contenido, List<String> abstracto) throws IOException{
+        try {
+            Directory directory = FSDirectory.open(Paths.get(filePath)); // Define where to save Lucene index
+
+            Analyzer defaultAnalyzer = new StandardAnalyzer();
+
+            Map<String, Analyzer> perFieldAnalyzers = new HashMap<>();
+            perFieldAnalyzers.put("Tittle", new WhitespaceAnalyzer());
+            perFieldAnalyzers.put("Authors", new WhitespaceAnalyzer());
+            perFieldAnalyzers.put("Abstract", new StandardAnalyzer());
+            perFieldAnalyzers.put("Content", new EnglishAnalyzer());
+
+
+            Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
+            IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(analyzer));
+            for (int i=0; i<titulos.size(); i++){
+                Document doc = new Document();
+                doc.add(new StringField("NameDoc", nombres.get(i), Field.Store.YES));
+                doc.add(new StringField("Tittle", titulos.get(i), Field.Store.YES));
+                doc.add(new StringField("Authors", autores.get(i), Field.Store.YES));
+                doc.add(new StringField("Abstract", abstracto.get(i), Field.Store.YES));
+                doc.add(new StringField("Content", contenido.get(i), Field.Store.YES));
+                writer.addDocument(doc);
+            }
+            writer.commit(); // persist changes to the disk
+            writer.close();
+
+        } catch (IOException e){
+            System.out.println(e.getMessage());
         }
-        IndexWriter writer = new IndexWriter(directory, config);
-        writer.deleteAll();
-
     }
-    }
+}
--- alexander/src/main/java/org/example/Main.java
+++ alexander/src/main/java/org/example/Main.java
@@ -27,13 +27,12 @@ import org.apache.lucene.store.FSDirectory;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //Directory directory = FSDirectory.open(Paths.get("../lucene-index")); // Define where to save Lucene index
-        //CREAR EL DIRECTORIO PARA ACCEDER AL LOS HTMLS
-
+        String path = "../lucene-index"; // Define where to save Lucene index
 
         //PASAR LOS HTMLS A LA FUNCION XPATH PARA QUE EXTRAIGA EL TITULO
         Path dirPath = Paths.get("../htmls2");
 
+        List<String> listaNombres = new ArrayList<>();
         List<String> listaTitulos = new ArrayList<>();
         List<String> listaContenido = new ArrayList<>();
         List<String> listaAutores = new ArrayList<>();
@@ -43,35 +42,24 @@ public class Main {
 
             for (Path filePath : stream) {
                 Xpaths extraer = new Xpaths(filePath); //llamo al archivo Xpaths
-                String titulos = filePath.getFileName() + " ---> " + extraer.titulos();
+                String nombres = filePath.getFileName() + "";
+                listaNombres.add(nombres);
+                String titulos = extraer.titulos();
                 listaTitulos.add(titulos);
-                String autores = filePath.getFileName() + " ---> " + extraer.autores();
+                String autores = extraer.autores();
                 listaAutores.add(autores);
-                //String contenido = filePath.getFileName() + " ---> " + extraer.contenido();
-                //listaContenido.add(contenido);
-                String abstracto = filePath.getFileName() + " ---> " + extraer.abstracto();
+                String contenido = extraer.contenido();
+                listaContenido.add(contenido);
+                String abstracto = extraer.abstracto();
                 listaAbstract.add(abstracto);
 
             }
         } catch (IOException e){
             System.out.println("Error al leer los archivos htmls" + e.getMessage());
         }
-        for (String a : listaAbstract) {
-            System.out.println(a);
-        }
-        /*
-        IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig()); // Define an IndexWriter
-        Document doc1 = new Document();
-        doc1.add(new TextField("titolo", "Come diventare un ingegnere dei dati, Data Engineer?", Field.Store.YES));
-        doc1.add(new TextField("contenuto", "Sembra che oggigiorno tutti vogliano diventare un Data Scientist ...", Field.Store.YES));
-        Document doc2 = new Document();
-        doc2.add(new TextField("titolo", "Curriculum Ingegneria dei Dati - Sezione di Informatica e Automazione", Field.Store.YES));
-        doc2.add(new TextField("contenuto", "Curriculum. Ingegneria dei Dati. Laurea Magistrale in Ingegneria Informatica ...", Field.Store.YES));
-        writer.addDocument(doc1); // add Documents to be indexed
-        writer.addDocument(doc2);
-        writer.commit(); // persist changes to the disk
-        writer.close();
-        */
+
+        Indexer index = new Indexer();
+        index.index(path, listaNombres, listaTitulos, listaAutores, listaAbstract, listaContenido);
         System.out.println("Ciao");
     }
 }
