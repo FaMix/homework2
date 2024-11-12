17dbf342bc45902547794ca6c2b9d0387754e20d
FaMix <45107582+FaMix@users.noreply.github.com>
2024-11-12 18:19:00 +0100

    First Commit Fabio Branch

--- src/homework2/homework2.java
+++ src/homework2/homework2.java
@@ -1,7 +1,18 @@
 package homework2;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Paths;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -10,10 +21,15 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.W3CDom;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class homework2 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
 		Directory directory = FSDirectory.open(Paths.get("../lucene-index")); // Define where to save Lucene index
 		
 		IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig()); // Define an IndexWriter
@@ -28,6 +44,34 @@ public class homework2 {
 		writer.commit(); // persist changes to the disk
 		writer.close();
 		
-		System.out.println("Ciao");
+        File input = new File("../htmls2/2107.01477.html");
+        
+        org.jsoup.nodes.Document jsoupDoc = Jsoup.parse(input, null);
+       
+        W3CDom w3cDom = new W3CDom();
+        org.w3c.dom.Document xmlDoc = w3cDom.fromJsoup(jsoupDoc);
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+
+        String authorExpression = "//span[@class='ltx_personname']//text()[not(ancestor::sup) and not( ancestor::span[contains(@class,'ltx_note')] ) and not(ancestor::math)]"; // Modifica l'espressione in base al tuo file HTML
+        XPathExpression xPathAuthorExpression = xpath.compile(authorExpression);
+        
+        String abstractExpression = "//div[@class='ltx_abstract']//text()";
+        XPathExpression xPathAbstractExpression = xpath.compile(abstractExpression);
+        
+        String contentExpression = "//section[@class='ltx_section']//div[@class='ltx_para']//text() | //section[@class='ltx_section']//h2[contains(@class,'ltx_title')]//text()";
+        XPathExpression xPathContentExpression = xpath.compile(contentExpression);
+
+        NodeList nodes = (NodeList) xPathContentExpression.evaluate(xmlDoc, XPathConstants.NODESET);
+
+        String result = "";
+        // Stampa i risultati
+        for (int i = 0; i < nodes.getLength(); i++) {
+        	result = result + nodes.item(i).getTextContent();
+        }
+		
+        //String output = result.replaceAll("\\r?\\n", "");
+        System.out.println(result);
 	}
 }
